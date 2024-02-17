3f8e88b3d462389ed6cfacd3be22c9b6894172a3
Jack Milman <jpm1228@gmail.com>
2024-02-16 23:09:42 -0500

    Progress on Milestone 1. Most of Serialization is finished, and many things are better organized.

--- GridKid/ast.rb
+++ GridKid/ast.rb
@@ -9,37 +9,31 @@ module Ast
         end
     end
 
-    class Integer < Primitive
-        def traverse(visitor, payload)
-            visitor.visit_integer(self, nil)
+    # Used as a wrapper for type checking.
+    class Number
+    end
+
+    class NumberP < Number
+        attr_reader :value
+
+        def initialize(value)
+            @value = value
         end
     end
 
-    class Float < Primitive
+    class IntP < NumberP
         def traverse(visitor, payload)
-            visitor.visit_float(self, nil)
+            visitor.visit_integer(self, payload)
         end
     end
 
-    class Boolean < Primitive
+    class FloatP < NumberP
         def traverse(visitor, payload)
-            visitor.visit_boolean(self, nil)
+            visitor.visit_float(self, payload)
         end
     end
 
-    class String < Primitive
-        def traverse(visitor, payload)
-            visitor.visit_string(self, nil)
-        end
-    end
-
-    class CellAddress < Primitive
-        def traverse(visitor, payload)
-            visitor.visit_cell_address(self, nil)
-        end
-    end
-
-    class BinaryOperator
+    class NumberBinary < Number
         attr_reader :left_operand
         attr_reader :right_operand
         
@@ -49,85 +43,208 @@ module Ast
         end
     end
 
-    class Add < BinaryOperator
+    class Add < NumberBinary
         def traverse(visitor, payload)
-            visitor.visit_add(self, nil)
+            visitor.visit_add(self, payload)
         end
     end
 
-    class Subtract
+    class Subtract < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_sub(self, payload)
+        end
     end
 
-    class Multiply
+    class Multiply < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_mult(self, payload)
+        end
     end
 
-    class Divide
+    class Divide < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_div(self, payload)
+        end
     end
 
-    class Modulo
+    class Modulo < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_modulo(self, payload)
+        end
     end
 
-    class Exponentiate
+    class Exponentiate < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_exp(self, payload)
+        end
     end
 
-    class Negate
+    class BitwiseAnd < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_bitwise_and(self, payload)
+        end
+    end
+    
+    class BitwiseOr < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_bitwise_or(self, payload)
+        end
     end
 
-    class LogicalAnd
+    class BitwiseXor < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_bitwise_xor(self, payload)
+        end
     end
 
-    class LogicalOr
+    class BitwiseNot < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_bitwise_not(self, payload)
+        end
     end
 
-    class LogicalNot
+    class BitwiseLeftShift < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_bitwise_left(self, payload)
+        end
+    end
+
+    class BitwiseRightShift < NumberBinary
+        def traverse(visitor, payload)
+            visitor.visit_bitwise_right(self, payload)
+        end
+    end
+
+    class NumberUnary < Number
+        attr_reader :operand
+        
+        def initialize(operand)
+            @operand
+        end
+    end
+
+    class Negate < NumberUnary
+        def traverse(visitor, payload)
+            visitor.visit_negate(self, payload)
+        end
+    end
+
+    class CastFloatToInt < NumberUnary
+        def traverse(visitor, payload)
+            visitor.visit_float_to_int(self, payload)
+        end
+    end
+
+    class CastIntToFloat < NumberUnary
+        def traverse(visitor, payload)
+            visitor.visit_int_to_float(self, payload)
+        end
+    end
+
+    # Used as a wrapper for type checking.
+    class Boolean
+    end
+
+    class BooleanP < Boolean
+        attr_reader :value
+
+        def initialize(value)
+            @value = value
+        end
+
+        def traverse(visitor, payload)
+            visitor.visit_boolean(self, payload)
+        end
+    end
+
+    class BooleanBinary < Boolean
+        attr_reader :left_operand
+        attr_reader :right_operand
+        
+        def initialize(left, right)
+            @left_operand = left
+            @right_operand = right
+        end
+    end
+
+    class And < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_and(self, payload)
+        end
+    end
+
+    class Or < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_or(self, payload)
+        end
+    end
+
+    class Equals < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_equals(self, payload)
+        end
+    end
+
+    class NotEquals < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_not_equals(self, payload)
+        end
+    end
+
+    class LessThan < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_less_than(self, payload)
+        end
+    end
+
+    class LessThanEqualTo < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_less_than_equal_to(self, payload)
+        end
+    end
+
+    class GreaterThan < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_greater_than(self, payload)
+        end
+    end
+
+    class GreaterThanEqualTo < BooleanBinary
+        def traverse(visitor, payload)
+            visitor.visit_greater_than_equal_to(self, payload)
+        end
+    end
+
+    class BooleanUnary < Boolean
+        attr_reader :operand
+        
+        def initialize(operand)
+            @operand
+        end
+    end
+
+    class Not < BooleanUnary
+        def traverse(visitor, payload)
+            visitor.visit_not(self, payload)
+        end
+    end
+
+    class StringP < Primitive
+        def traverse(visitor, payload)
+            visitor.visit_string(self, payload)
+        end
+    end
+
+    class CellAddressP < Primitive
+        def traverse(visitor, payload)
+            visitor.visit_cell_address(self, payload)
+        end
     end
 
     class CellLValue
     end
 
     class CellRValue
-    end
-
-    class BitwiseAnd
-    end
-    
-    class BitwiseOr
-    end
-
-    class BitwiseXor
-    end
-
-    class BitwiseNot
-    end
-
-    class BitwiseLeftShift
-    end
-
-    class BitwiseRightShift
-    end
-
-    class Equals
-    end
-
-    class NotEquals
-    end
-
-    class LessThan
-    end
-
-    class LessThanEqualTo
-    end
-
-    class GreaterThan
-    end
-
-    class GreaterThanEqualTo
-    end
-
-    class CastFloatToInt
-    end
-
-    class CastIntToFloat
     end
 
     class Max
@@ -144,38 +261,245 @@ module Ast
 
     class Serializer
         def visit_integer(node, payload)
-            return node
+            # checking against default Ruby type
+            if !node.value.is_a?(Integer)
+                raise TypeError, "Not an integer: #{node.value}"
+            end
+            return "#{node.value}"
         end
         
         def visit_float(node, payload)
-            return node
+            # duck typing with modulo
+            if !node.value.respond_to?(:%)
+                raise TypeError, "Not a Float or subtype of Float: #{node.value}"
+            end
+            return "#{node.value}"
         end
         
         def visit_boolean(node, payload)
-            return node
+            # checking against default Ruby types
+            if !(node.value == true || node.value == false)
+                raise TypeError, "Not a boolean type: #{node.value}"
+            end
+            return "#{node.value}"
         end
         
         def visit_string(node, payload)
-            return node
+            # checking against default Ruby type
+            if !node.value.is_a?(String)
+                raise TypeError, "Not a string: #{node.value}"
+            end
+            return "#{node.value}"
         end
         
         def visit_cell_address(node, payload)
-            return node
-        end
-    
-        def visit_add(node, payload)
-            left_op = node.left_operand.traverse(self, nil)
-            if left_op.is_a?(Ast::Integer)
-                right_op = node.right_operand.traverse(self, nil)
-                if right_op.is_a?(Ast::Integer)
-                    sum = left_op.value + right_op.value
-                    return Ast::Integer.new(left_op.value + right_op.value)
-                else
-                    raise "Bad right go home"
-                end
-            else
-                raise "Bad left go home"
+            # checking against default Ruby type
+            if !node.value.is_a?(Integer)
+                raise TypeError, "Not a valid address: #{node.value}"
             end
+            return "#{node.value}"
         end
+
+        # Helper method for binary operations
+        def visit_binary(node, operation, payload)
+            left_op = node.left_operand
+            right_op = node.right_operand
+            return "(#{left_op.traverse(self, payload)} #{operation} #{right_op.traverse(self, payload)})"
+        end
+
+        # Helper method for unary operations
+        def visit_unary(node, operation, payload)
+            op = node.operand
+            return "(#{operation}#{op.traverse(self, payload)})"
+        end
+
+        def validate_numeric_binary(node, payload)
+            good = false
+            left_op = node.left_operand
+            good = left_op.is_a?(Number)
+            if !good
+                raise TypeError, "Incompatible left operand"
+            end
+            right_op = node.right_operand
+            good = right_op.is_a?(Number)
+            if !good
+                raise TypeError, "Incompatible right operand"
+            end
+            return good
+        end
+
+        def validate_numeric_unary(node, payload)
+            good = false
+            op = node.operand
+            good = op.is_a?(Number)
+            if !good
+                raise TypeError, "Incompatible operator"
+            end
+            return good
+        end
+
+        def visit_add(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "+", payload)
+        end
+
+        def visit_sub(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "-", payload)
+        end
+
+        def visit_mult(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "*", payload)
+        end
+
+        def visit_div(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "/", payload)
+        end
+
+        def visit_modulo(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "%", payload)
+        end
+
+        def visit_exp(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "^", payload)
+        end
+
+        def visit_bitwise_and(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "&", payload)
+        end
+
+        def visit_bitwise_or(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "|", payload)
+        end
+        
+        def visit_bitwise_xor(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "^", payload)
+        end
+
+        def visit_bitwise_not(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "~", payload)
+        end
+
+        def visit_bitwise_left(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "<<", payload)
+        end
+
+        def visit_bitwise_right(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, ">>", payload)
+        end
+
+        def visit_negate(node, payload) # TODO: UNFINISHED AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            validate_numeric_unary(node, payload)
+            visit_unary(node, "-", payload)
+        end
+
+        # Helper method for logical binary operations
+        def validate_logical_binary(node, payload)
+            good = false
+            left_op = node.left_operand
+            good = left_op.is_a?(Boolean)
+            if !good
+                raise TypeError, "Incompatible left operand"
+            end
+            right_op = node.right_operand
+            good = right_op.is_a?(Boolean)
+            if !good
+                raise TypeError, "Incompatible right operand"
+            end
+            return good
+        end
+
+        def validate_logical_unary(node, payload)
+            good = false
+            op = node.operand
+            good = op.is_a?(Boolean)
+            if !good
+                raise TypeError, "Incompatible operator"
+            end
+            return good
+        end
+
+        def visit_and(node, payload)
+            validate_logical_binary(node, payload)
+            visit_binary(node, "and", payload)
+        end
+
+        def visit_or(node, payload)
+            validate_logical_binary(node, payload)
+            visit_binary(node, "or", payload)
+        end
+
+        def visit_not(node, payload) # TODO: UNFINISHED AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            validate_logical_unary(node, payload)
+            visit_unary(node, "not ", payload)
+        end
+
+        def visit_equals(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "==", payload)
+        end
+        
+        def visit_not_equals(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "!=", payload)
+        end
+        
+        def visit_less_than(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "!=", payload)
+        end
+        
+        def visit_less_than_equal_to(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "!=", payload)
+        end
+        
+        def visit_greater_than(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "!=", payload)
+        end
+        
+        def visit_greater_than_equal_to(node, payload)
+            validate_numeric_binary(node, payload)
+            visit_binary(node, "!=", payload)
+        end
+        
+        def visit_float_to_int(node, payload)
+            # good = false
+            # op = node.operand
+            # good = op.is_a?(Number)
+            # if !good
+            #     raise TypeError, "Incompatible operator"
+            # end
+            # return "(float(#{right_op.traverse(self, payload)}))"
+            validate_numeric_unary(node, payload)
+            visit_unary(node, "-", payload)
+        end
+
+        def visit_float_to_int(node, payload)
+            # good = false
+            # op = node.operand
+            # good = op.is_a?(Number)
+            # if !good
+            #     raise TypeError, "Incompatible operator"
+            # end
+            # return "(int(#{right_op.traverse(self, payload)}))"
+            validate_numeric_unary(node, payload)
+            visit_unary(node, "-", payload)
+        end
+    end
+
+    class Evaluator
+
     end
 end
--- GridKid/main.rb
+++ GridKid/main.rb
@@ -1,7 +1,154 @@
 # #!usr/bin/env ruby
 require_relative './ast.rb'
 
-added = Ast::Add.new(Ast::Integer.new(1), Ast::Integer.new(2))
+ser = Ast::Serializer.new
 
-text = added.traverse(Ast::Serializer.new, nil)
-print (text)
+int1 = Ast::IntP.new(2)
+int2 = Ast::IntP.new(-2)
+intBad = Ast::IntP.new(4.5)
+int_result1 = int1.traverse(ser, nil)
+puts "Int 1: #{int_result1}"
+int_result2 = int2.traverse(ser, nil)
+puts "Int 2: #{int_result2}"
+begin
+int_resultBad = intBad.traverse(ser, nil)
+puts "Int Bad: #{int_resultBad}"
+rescue TypeError
+    puts "Bad Int detected"
+end
+
+
+bool1 = Ast::BooleanP.new(true)
+bool2 = Ast::BooleanP.new(false)
+boolBad = Ast::BooleanP.new(3)
+bool_result1 = bool1.traverse(ser, nil)
+puts "Bool 1: #{bool_result1}"
+bool_result2 = bool2.traverse(ser, nil)
+puts "Bool 2: #{bool_result2}"
+begin
+bool_resultBad = boolBad.traverse(ser, nil)
+puts "Bool Bad: #{bool_resultBad} Failed to detect"
+rescue TypeError
+    puts "Bad Bool detected"
+end
+
+
+float1 = Ast::FloatP.new(2.0)
+float2 = Ast::FloatP.new(-2.7)
+floatBad1 = Ast::IntP.new(false)
+floatBad2 = Ast::IntP.new("blablabla")
+float_result1 = float1.traverse(ser, nil)
+puts "Float 1: #{float_result1}"
+float_result2 = float2.traverse(ser, nil)
+puts "Float 2: #{float_result2}"
+begin
+float_resultBad1 = floatBad1.traverse(ser, nil)
+puts "Float Bad: #{float_resultBad1} Failed to detect"
+rescue TypeError
+    puts "Bad Float 1 detected"
+end
+begin
+float_resultBad2 = floatBad2.traverse(ser, nil)
+puts "Float Bad: #{float_resultBad2} Failed to detect"
+rescue TypeError
+    puts "Bad Float 2 detected"
+end
+
+
+add1 = Ast::Add.new(
+    Ast::IntP.new(1), 
+    Ast::IntP.new(2)
+    )
+add2 = Ast::Add.new(
+    Ast::FloatP.new(1.5), 
+    Ast::IntP.new(2)
+    )
+add3 = Ast::Add.new(
+    Ast::FloatP.new(1.5), 
+    Ast::FloatP.new(2.5)
+    )
+add4 = Ast::Add.new(
+    Ast::IntP.new(1), 
+    Ast::FloatP.new(2.5)
+    )
+compound_add = Ast::Add.new(
+        Ast::Add.new(
+            Ast::FloatP.new(0.5), 
+            Ast::FloatP.new(2.5)
+        ),
+        Ast::IntP.new(3)
+    )
+bAdd = Ast::Add.new( # get it? Because bad
+    Ast::BooleanP.new(true), 
+    Ast::FloatP.new(2.5)
+    )
+add_result1 = add1.traverse(ser, nil)
+puts "Add 1: #{add_result1}"
+add_result2 = add2.traverse(ser, nil)
+puts "Add 2: #{add_result2}"
+add_result3 = add3.traverse(ser, nil)
+puts "Add 3: #{add_result3}"
+add_result4 = add4.traverse(ser, nil)
+puts "Add 4: #{add_result4}"
+compound_result = compound_add.traverse(ser, nil)
+puts "Add Compound: #{compound_result}"
+begin
+bAdd_result = bAdd.traverse(ser, nil)
+puts "bAdd: #{bAdd_result}"
+rescue TypeError
+    puts "bAdd TypeError detected"
+end
+
+
+mult1 = Ast::Multiply.new(
+    Ast::IntP.new(7),
+    Ast::FloatP.new(3)
+)
+mult2 = Ast::Multiply.new(
+    Ast::Add.new(
+        Ast::IntP.new(1),
+        Ast::FloatP.new(2)
+    ),
+    Ast::IntP.new(3)
+)
+mult_prod1 = mult1.traverse(ser, nil)
+puts "Mult 1: #{mult_prod1}"
+mult_prod2 = mult2.traverse(ser, nil)
+puts "Mult 2: #{mult_prod2}"
+
+
+
+# TODO-list:
+# EVALUATOR HOLY MOLY YOU GOTTA DO THE EVALUATOR EVERYTHING NEEDS THAT
+# GOTTA MAKE THE GRID YOU CAN'T HAVE A GRIDKID WITHOUT A GRID THAT'S LIKE A BATMAN WITHOUT A JOKER
+
+# String                --tests and structure
+# Cell Address          --tests and structure
+# Subtraction           --tests
+# Division              --tests
+# Modulo                --tests
+# Exponentiation        --tests
+# Negation              --tests
+# And                   --tests
+# Or                    --tests
+# Not                   --tests
+# Cell Lvalues          --tests and structure
+# Cell Rvalues          --tests and structure
+# Bitwise And           --tests
+# Bitwise Or            --tests
+# Bitwise Xor           --tests
+# Bitwise Not           --tests
+# Bitwise Left Shift    --tests
+# Bitwise Right Shift   --tests
+# Equal                 --tests
+# Not Equal             --tests
+# Less Than             --tests
+# Less Than Equal To    --tests
+# Greater Than          --tests
+# Greater Than Equal To --tests
+# Float-to-int          --tests
+# Int-to-float          --tests
+# Max                   --tests and structure
+# Min                   --tests and structure
+# Mean                  --tests and structure
+# Sum                   --tests and structure
