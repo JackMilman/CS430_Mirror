7049404c3db3ed400de3be80afd2b62d185ba078
Jack Milman <milmanjp@dukes.jmu.edu>
2024-03-28 21:43:07 -0400

    Added simple Curses calls to delegate work to the Program in interface.rb and clean up once it's done

--- GridKid/main.rb
+++ GridKid/main.rb
@@ -2,6 +2,7 @@
 require 'curses'
 require_relative './ast.rb'
 require_relative './interp.rb'
+require_relative './interface.rb'
 require_relative 'ast_tests/arithmetic_tests.rb'
 require_relative 'ast_tests/bitwise_tests.rb'
 require_relative 'ast_tests/logical_tests.rb'
@@ -10,6 +11,7 @@ require_relative 'ast_tests/cell_tests.rb'
 
 include Ast
 include Curses
+include Interface
 
 eval = Ast::Evaluator.new
 ser = Ast::Serializer.new
@@ -43,16 +45,16 @@ ser = Ast::Serializer.new
 # LogicalTests::Test.new.run_tests_serial(ser, Ast::Runtime.new)
 # puts
 
-puts "------CELL SERIALIZATION TESTS------"
-CellTests::Test.new.run_tests_serial(ser, Ast::Runtime.new)
-puts
+# puts "------CELL SERIALIZATION TESTS------"
+# CellTests::Test.new.run_tests_serial(ser, Ast::Runtime.new)
+# puts
 
 # puts "------CELL EVALUATION TESTS------"
 # CellTests::Test.new.run_tests_eval(eval, ser, Ast::Runtime.new(3))
 
 init_screen
-getch
-setpos(1, 5)
-addstr("total:")
-getch
+
+prog = Program.new
+prog.main_loop
+
 close_screen
