6937582ca644195af4c22035237032fb9fa86c0a
Jack Milman <milmanjp@dukes.jmu.edu>
2024-03-29 11:09:55 -0400

    Improved presentation of the grid. Refactored cg_refresh to be cleaner

--- GridKid/interface.rb
+++ GridKid/interface.rb
@@ -2,14 +2,16 @@ require 'curses'
 include Curses
 
 module Interface
+    $pos_x = 0
+    $pos_y = 0
+    $grid_size = 10
 
     class Program
         def initialize
             @height = Curses.lines
             @width = Curses.cols
-            @pos_x = 0
-            @pos_y = 0
             @main_window = Window.new(@height, @width, 0, 0)
+            @main_window.keypad(true)
             @form = FormulaEditor.new(2, @width, 0, 0)
             @display = CellDisplay.new(2, @width, 2, 0)
             @grid = CellGrid.new(@height - 4, @width, 4, 0)
@@ -22,12 +24,21 @@ module Interface
                 if char == 'q'
                     break
                 # elsifs for other event triggers
+                elsif char == Key::LEFT
+                    $pos_x = $pos_x > 0 ? $pos_x - 1 : 0
+                elsif char == Key::RIGHT
+                    $pos_x = $pos_x < $grid_size ? $pos_x + 1 : 0
+                elsif char == Key::UP
+                    $pos_y = $pos_y > 0 ? $pos_y - 1 : 0
+                elsif char == Key::DOWN
+                    $pos_y = $pos_y < $grid_size ? $pos_y + 1 : 0
                 end
-        
+                
                 # draw and refresh affected windows
                 @form.f_refresh
                 @display.cd_refresh
                 @grid.cg_refresh
+                @main_window.setpos(0, @width - 50)
             end
         end
     
@@ -46,8 +57,8 @@ module Interface
 
         def f_refresh
             @id_w.setpos(0, 0)
-            @id_w.addstr("Formula [0, 0]")
-            @id_w.setpos(0, @id_w_len - 10)
+            @id_w.addstr("Formula [#{$pos_y}, #{$pos_x}]")
+            @id_w.setpos(0, @id_w_len - 1)
             @id_w.addstr("\u2502")
             horizontal_line(@id_w, 1, 0, @id_w_len)
             horizontal_line(@w, 1, 0, @width)
@@ -69,8 +80,8 @@ module Interface
 
         def cd_refresh
             @id_w.setpos(0, 0)
-            @id_w.addstr("Value [0, 0]")
-            @id_w.setpos(0, @id_w_len - 10)
+            @id_w.addstr("Value [#{$pos_y}, #{$pos_x}]")
+            @id_w.setpos(0, @id_w_len - 1)
             @id_w.addstr("\u2502")
             horizontal_line(@id_w, 1, 0, @id_w_len)
             horizontal_line(@w, 1, 0, @width)
@@ -85,34 +96,31 @@ module Interface
             @height = height
             @width = width
             @w = Window.new(@height, @width, r_ind, c_ind)
-            @col_first = 8 # arbitrarily chosen spacing
-            @col_step = 10 # arbitrarily chosen spacing
-            @row_first = 1
+            @col_first = 4 # arbitrarily chosen spacing, half of col_step
+            @col_step = 8 # arbitrarily chosen spacing
+            @row_max = ($grid_size * 2) + 1
         end
 
         def cg_refresh
             i = 0
-            (@col_first...(@width - 1)).step(@col_step) do |column| # populates column indices
-                @w.setpos(0, column)
-                @w.addstr("#{i}")
-                i += 1
+            (1..@row_max).each do |row|
+                if row % 2 == 0
+                    @w.setpos(row, 2)
+                    @w.addstr("#{i}")
+                    i += 1
+                else
+                    horizontal_line(@w, row, 0, @width)
+                end
             end
-            ((@col_first / 2)...(@width)).step(@col_step) do |column| # populates column-separators
-                vertical_line(@w, column, 0, @height)
-                @w.setpos(2, column + 1)
-                @w.addstr("#{(column + 1234.5678910111213).truncate(@col_step - 1)}") # TODO: REMOVE THIS, IT IS A TEMPORARY SOLUTION
-            end
-            
 
-            (@row_first...(@height)).step(2) do |row| # populates row-separators
-                horizontal_line(@w, row, 0, @width)
+
+            (0...$grid_size).each do |col| # populates column-indices and vertical-separators
+                @w.setpos(0, @col_step + (col * @col_step))
+                @w.addstr("#{col}")
+                vertical_line(@w, @col_first + (col * @col_step), 0, @row_max + 1)
             end
-            i = 0
-            ((@row_first + 1)...(@height - 1)).step(2) do |row| # populates row indices
-                @w.setpos(row, 0)
-                @w.addstr("#{i}")
-                i += 1
-            end
+            vertical_line(@w, @col_first + ($grid_size * @col_step), 0, @row_max + 1)
+
             @w.refresh
         end
     end
